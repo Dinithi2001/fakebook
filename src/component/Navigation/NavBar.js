9aafd7e58f0ff5865bb2ec9279b4aebfbec0077f
Dinithi2001 <149286247+Dinithi2001@users.noreply.github.com>
2024-07-09 16:17:39 +0530

    change the navigation bar

--- src/component/Navigation/NavBar.js
+++ src/component/Navigation/NavBar.js
@@ -1,42 +1,63 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
+import WidgetsRoundedIcon from '@mui/icons-material/WidgetsRounded';
 import MessageIcon from '@mui/icons-material/Message';
-import SettingsIcon from '@mui/icons-material/Settings';
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
+import FacebookIcon from '@mui/icons-material/FacebookRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import OndemandVideoRoundedIcon from '@mui/icons-material/OndemandVideoRounded';
+import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
+import GroupsRoundedIcon from '@mui/icons-material/GroupsRounded';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import "./navbar.css";
 
 export default function NavBar() {
   return (
       <div className="navBarBox">
-
+      
         <div className="navBarLeft">
-           <span className="logo">fakebook</span> 
+           <div className="logoIcon">
+                <FacebookIcon className='logo'></FacebookIcon>
+            </div> 
+            <div className="searchBarBox">
+                <SearchIcon className='searchIcon' />
+                <input placeholder="Search fakebook" className="searchInput" />
+            </div>
         </div>
 
         <div className="navBarCenter">
-            <div className="searchBarBox">
-                <SearchIcon className='searchIcon' />
-                <input placeholder="Search for your friend" className="searchInput" />
+            <div className="centerIcons">
+                <HomeRoundedIcon></HomeRoundedIcon>
             </div>
+            <div className="centerIcons">
+                <OndemandVideoRoundedIcon></OndemandVideoRoundedIcon>
+            </div>
+            <div className="centerIcons">
+                <StoreRoundedIcon></StoreRoundedIcon>
+            </div>
+            <div className="centerIcons">
+                <GroupsRoundedIcon></GroupsRoundedIcon>
+            </div>
+            <div className="centerIcons">
+                <MenuRoundedIcon></MenuRoundedIcon>
+            </div>
+            
         </div>
+        
 
         <div className="navBarRight">
-            <div className="navBarLinks">
-                <span className="navBarLink">Home</span>
-                <span className="navBarLink">Profile</span>
-                
-            </div>
+            
             <div className="navBarIcons">
                 <div className="navBarIcon">
-                    <PersonIcon></PersonIcon>
-                    <span className="iconTag">3</span>
+                    <WidgetsRoundedIcon></WidgetsRoundedIcon>
+                   
                 </div>
                 <div className="navBarIcon">
                     <MessageIcon></MessageIcon>
                     <span className="iconTag">5</span>
                 </div>
                 <div className="navBarIcon">
-                    <SettingsIcon></SettingsIcon>
+                    <NotificationsRoundedIcon></NotificationsRoundedIcon>
                     <span className="iconTag">2</span>
                 </div>
 
